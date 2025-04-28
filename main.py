54f9644eb07a7d71ce68605726a28140f51bec5b
Akshitdhake2004 <akshitjayeshdhake@gmail.com>
2025-04-28 23:37:37 +0530

    adding the updated docker file

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ logging.basicConfig(level=logging.DEBUG)
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
